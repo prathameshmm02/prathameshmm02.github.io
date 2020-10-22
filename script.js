7421227757c7b645df8b6a1dad6cecb5b0cf5a32
Prathamesh More <prathameshmm02@gmail.com>
2020-10-22 12:31:50 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var sects = document.getElementsByTagName("section");
+var navbuts = document.getElementsByTagName("a");
+function actInact() {
+  for (i = 0; i < 4; i++) {
+    sects[i].style.display = "none";
+    navbuts[i].classList.remove("active");
+    navbuts[i].classList.add("inactive");
+  }
+}
+function showSect(num) {
+  actInact();
+  sects[num].style.display = "block";
+  navbuts[num].classList.remove("inactive");
+  navbuts[num].classList.add("active");
+}
+function showId() {
+  document.getElementById("disId").style.opacity = 1;
+}
